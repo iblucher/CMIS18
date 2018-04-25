52076f2df14ad5185f60b2c52ffa2d605b80b0f1
Isabela Blucher <isabela.blucher@usp.br>
2018-04-25 16:15:43 +0200

    Finished matrix assembly

--- toy_problem_2D.m
+++ toy_problem_2D.m
@@ -3,11 +3,12 @@ dx = 1;
 dy = 1;
 kappa = 2;
 n = 4;
+N = n + 2;
 
 % Compute f
-f = zeros(n, n);
-for i = 1:n+2
-    for j = 1:n+2
+f = zeros(N);
+for i = 2:N-1
+    for j = 2:N-1
         f(i, j) = i + j;
     end
 end
@@ -16,48 +17,62 @@ imagesc(f)
 
 % Compute A
 % Compute coefficients
-c0 = -kappa^2 - 2/dx^2 - 2/dy^2;
-c1 = 1/dx^2;
-c2 = 1/dy^2;
+c0 = -kappa^2 - 2.0/dx^2 - 2.0/dy^2;
+c1 = 1.0/dx^2;
+c2 = 1.0/dy^2;
 
-% Initialize domain nodes coefficient
-N = (n + 2)^2;
-A = zeros(N);
-for j = 2:n+1
-    for i = 2:n+1
-        k = j*n + i;
+% Initialize domain nodes coefficients
+A = zeros(N^2);
+for j = 2:N-1
+    for i = 2:N-1
+        k = (j-1)*N + i;
         A(k, k) = c0;
         A(k, k - 1) = c1;
         A(k, k + 1) = c1;
-        A(k, k - n) = c2;
-        A(k, k + n) = c2;
+        A(k, k - N) = c2;
+        A(k, k + N) = c2;
     end
 end
 
 % Initialize ghost nodes coefficients
-for i = 2:n+1
-    k1 = 1*n + i;
-    k2 = 6*n + i;
+for i = 2:N-1
+    j1 = 1;
+    j2 = 6;
+    k1 = (j1 - 1) * N + i;
+    k2 = (j2 - 1) * N + i;
     A(k1, k1) = 1;
-    A(k1, k1 + n) = -1;
+    A(k1, k1 + N) = -1;
     A(k2, k2) = 1;
-    A(k2, k2 - n) = -1;
+    A(k2, k2 - N) = -1;
 end
 
-for j = 2:n+1
-    k3 = j*n + 1;
-    k4 = j*n + 6;
+for j = 2:N-1
+    i1 = 1;
+    i2 = 6;
+    k3 = (j-1)*N + i1;
+    k4 = (j-1)*N + i2;
     A(k3, k3) = 1;
     A(k3, k3 + 1) = -1;
     A(k4, k4) = 1;
     A(k4, k4 - 1) = -1;
 end
 
-f_new = reshape(f, [(n+2)^2, 1]);
+A(1, 1) = 10;
+A(N, N) = 10;
+A(N*N, N*N) = 10;
+A(N*(N-1) + 1, N*(N-1) + 1) = 10;
+
+f_new = reshape(f, [(N)^2, 1]);
+
+imagesc(A)
 
 % Compute solution for Au = f
 
 u = A \ f_new;
+
+
+% Compute eigenvalues
+e = eig(A);
 
 
            
